df176e963012a8c989940e3579216ad70c1fa55f
Tony S Yu <tsyu80@gmail.com>
2017-04-29 11:35:19 -0500

    Use bleach.clean to sanitize markup

--- scrappyr/utils/markup.py
+++ scrappyr/utils/markup.py
@@ -1,18 +1,24 @@
 import bleach
+import bleach.sanitizer
 from markdown import markdown
 
+
+ALLOWED_TAGS = bleach.sanitizer.ALLOWED_TAGS + [
+    'div', 'p',
+    'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
+]
+
+
+def _render_markdown(untrusted_text):
+    return bleach.clean(markdown(untrusted_text), tags=ALLOWED_TAGS)
+
+
 MARKDOWN = 'markdown'
-
 _MARKUP_CONVERTER = {
-    MARKDOWN: markdown,
+    MARKDOWN: _render_markdown,
 }
-
 LANGUAGES = _MARKUP_CONVERTER.keys()
 DEFAULT_LANGUAGE = MARKDOWN
-
-
-def _render_markdown(text):
-    return bleach.clean(markdown(untrusted_text))
 
 
 def html_renderer_for(markup_language):
